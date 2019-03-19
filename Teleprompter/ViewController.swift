608501595a5b39bd1be57c11c05644ebf7a080ca
Josiah Agosto <agosto_josiah@yahoo.com>
2019-03-19 10:47:14 -0400

    Initial Commit

---
+++ Teleprompter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Teleprompter
+//
+//  Created by Josiah Agosto on 3/19/19.
+//  Copyright © 2019 Josiah Agosto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
